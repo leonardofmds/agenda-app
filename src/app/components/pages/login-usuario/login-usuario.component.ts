afc7bdcf49117e59deac836caf9d88e2e2ebf107
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-11 12:59:33 -0300

    first commit

---
+++ src/app/components/pages/login-usuario/login-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-usuario',
+  imports: [],
+  templateUrl: './login-usuario.component.html',
+  styleUrl: './login-usuario.component.css'
+})
+export class LoginUsuarioComponent {
+
+}
