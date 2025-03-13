85451ef1d0c111f365cdbd48a56b15564b065979
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-03-13 08:22:42 -0300

    Criação das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { CadastroUsuarioComponent } from './components/pages/cadastro-usuario/cadastro-usuario.component';
+import { LoginUsuarioComponent } from './components/pages/login-usuario/login-usuario.component';
+import { NotFoundComponent } from './components/errors/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', //rota default do projeto
+        redirectTo: '/pages/login-usuario', //redireciona para a rota /pages/cadastro-usuarios,
+        pathMatch: 'full' //se a rota for vazia, redireciona para /pages/cadastro-usuarios
+    },
+    {
+        path: 'pages/cadastro-usuario',
+        component: CadastroUsuarioComponent
+    },
+    {
+        path: 'pages/login-usuario',
+        component: LoginUsuarioComponent
+    },
+    {
+        path: '**', //rota para página não encontrada (404)
+        component: NotFoundComponent
+    }
+];
