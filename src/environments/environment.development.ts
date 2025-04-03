c032e27c3db343fb8c57cf313edcb50133ba54f9
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-04-03 09:22:30 -0300

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
-const baseUrl = 'https://gf7bdg0n-5073.brs.devtunnels.ms'
+const baseUrl = 'https://9h5p5djj-5073.brs.devtunnels.ms'
 
 export const environment = {
     apiUsuarios: `${baseUrl}/api/usuarios`,
