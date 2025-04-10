69e6127e08bb286b7e79377853495f58537d8a8f
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-04-10 09:19:33 -0300

    Cadastro de tarefa

--- src/app/modules/admin/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
+++ src/app/modules/admin/components/pages/cadastro-tarefas/cadastro-tarefas.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TarefasService } from '../../../services/tarefas.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { TarefasRequest } from '../../../models/tarefas.request';
+
 
 @Component({
   selector: 'app-cadastro-tarefas',
@@ -8,4 +12,39 @@ import { Component } from '@angular/core';
 })
 export class CadastroTarefasComponent {
 
+
+  form: FormGroup;
+  msg: string = '';
+
+
+  constructor(
+    private service: TarefasService,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      nome: new FormControl('', [Validators.required]),
+      data: new FormControl('', [Validators.required]),
+      prioridade: new FormControl('', [Validators.required]),
+    });
+  }
+
+
+  onSubmit() {
+
+
+    const request: TarefasRequest = {
+      nome: this.form.get('nome')?.value as string,
+      data: this.form.get('data')?.value as string,
+      prioridade: this.form.get('prioridade')?.value as string,
+    }
+
+
+    this.service.criar(request)
+      .subscribe({
+        next: (response) => {
+          this.msg = `Tarefa '${response.nome}' criada com sucesso!`;
+          this.form.reset();
+        }
+      });
+  }
 }
