45accd052471b6ee4c0000296e3b1ece0e5b99f9
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-04-10 09:18:40 -0300

    Autenticação com token

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -17,11 +17,27 @@ export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
     const authService = inject(AuthService);
 
 
-    //TODO Implementar o interceptor para adicionar o token de autenticação
+    //verificar se requisição é para um endpoint que exige token
+    const requerToken = endpointsQueExigemToken
+        .some(endpoint => req.url.includes(endpoint));
+
+
+    //se verdadeiro, adicionar o token no header da requisição
+    if(requerToken) {
+        //capturar os dados do usuário autenticado
+        const usuario = authService.get();
+        //clonar a requisição e adicionar o token JWT no cabecalho
+        const reqClone = req.clone({
+            setHeaders: {
+                Authorization: `Bearer ${usuario?.token}`
+            }
+        });
+
+
+        //enviando a requisição clonada
+        return next(reqClone);
+    }
 
 
     return next(req);
 }
-
-
-
