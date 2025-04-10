7b92ed40c0254eebcff28229d0329b670a9836fc
Leonardo Menezes <leonardomenezes.dev@gmail.com>
2025-04-10 09:19:45 -0300

    Consulta de tarefa

--- src/app/modules/admin/components/pages/consulta-tarefas/consulta-tarefas.component.ts
+++ src/app/modules/admin/components/pages/consulta-tarefas/consulta-tarefas.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TarefasService } from '../../../services/tarefas.service';
+import { TarefasResponse } from '../../../models/tarefas.response';
+
 
 @Component({
   selector: 'app-consulta-tarefas',
@@ -7,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrl: './consulta-tarefas.component.css'
 })
 export class ConsultaTarefasComponent {
+
+
+  tarefas: TarefasResponse[] = [];
+  msg: string = '';
+
+
+  constructor(
+    private service: TarefasService
+  ) {    
+  }
+
+
+  ngOnInit() {
+    this.service.listar()
+      .subscribe((response) => {
+        this.tarefas = response;
+      });
+  }
+
+
+  onDelete(id: string) {
+    if(confirm('Deseja realmente excluir a tarefa selecionada?')) {
+      this.service.excluir(id)
+        .subscribe((response) => {
+          this.msg = `Tarefa '${response.nome}', excluída com sucesso.`;
+          this.ngOnInit();
+        });
+    }
+  }
+
 
 }
